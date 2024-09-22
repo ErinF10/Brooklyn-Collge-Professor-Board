3fafddc886e9ffabaceef8b2070001feace9c893
Erin Forrest <erinforrest11@gmail.com>
2024-09-22 13:00:16 -0400

    Added in final styling touches and added in more Professors

--- Community_Board/src/App.jsx
+++ Community_Board/src/App.jsx
@@ -11,6 +11,10 @@ import block from "./assets/images/block.png"
 import goodson from "./assets/images/goodson.png"
 import benes from "./assets/images/benes.png"
 import nguyen from "./assets/images/nguyen.png"
+import rudowsky from "./assets/images/rudowsky.png"
+import selvan from "./assets/images/selvan.png"
+import taylan from "./assets/images/taylan.png"
+import xiang from "./assets/images/xiang.png"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -22,17 +26,22 @@ function App() {
       </header>
 
       <div className='professor-container'>
-        
+
         <h2>Computer Science Professors</h2>
         <div className='cs'>
-          <Professor name="Noson Yanofsky" rating="4.1/5.0" image={yanofsky} link="https://www.ratemyprofessors.com/professor/78366"/>
           <Professor name="Priyanka Samanta" rating="4.8/5.0" image={samanta} link="https://www.ratemyprofessors.com/professor/2757901" />
+          <Professor name="Basak Taylan" rating="4.5/5.0" image={taylan} link="https://www.ratemyprofessors.com/professor/2363018" />
+          <Professor name="Panneer Selvan" rating="5.0/5.0" image={selvan} link="https://www.ratemyprofessors.com/professor/2995207" />
+          <Professor name="Katherine Chuang" rating="2.4/5.0" image={chuang} link="https://www.ratemyprofessors.com/professor/2498806" />
           <Professor name="Moche Lach" rating="4.7/5.0" image={none} link="https://www.ratemyprofessors.com/professor/2538695" />
+          <Professor name="Noson Yanofsky" rating="4.1/5.0" image={yanofsky} link="https://www.ratemyprofessors.com/professor/78366"/>
+          <Professor name="Carlos Cuevas" rating="4.4/5.0" image={cuevas} link="https://www.ratemyprofessors.com/professor/2974235" />
+          <Professor name="Yongqing Xiang" rating="3.4/5.0" image={xiang} link="https://www.ratemyprofessors.com/professor/493854" />
+          <Professor name="Ira Rudowsky" rating="4.3/5.0" image={rudowsky} link="https://www.ratemyprofessors.com/professor/296621" />
           <Professor name="Gerald Weiss" rating="4.6/5.0" image={none} link="https://www.ratemyprofessors.com/professor/510797"  />
           <Professor name="Rebecca Levitan" rating="3.2/5.0" image={levitan} link="https://www.ratemyprofessors.com/professor/2009423" />
           <Professor name="Miriam Briskman" rating="4.8/5.0" image={none} link="https://www.ratemyprofessors.com/professor/2699760" />
-          <Professor name="Carlos Cuevas" rating="4.4/5.0" image={cuevas} link="https://www.ratemyprofessors.com/professor/2974235" />
-          <Professor name="Katherine Chuang" rating="2.4/5.0" image={chuang} link="https://www.ratemyprofessors.com/professor/2498806" />
+          
         </div>
 
         <h2>Mathematics Professors</h2>
